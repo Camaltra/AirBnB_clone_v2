387276422901b36abaa3e4c271b5fce0497db60c
Camaltra <mickael.boillaud@gmail.com>
2022-04-11 14:43:09 +0000

    Adding last feature w/ Philippe Willot <ph.willot@gmail.com>

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, storageType
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    if storageType == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
